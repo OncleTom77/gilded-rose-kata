fede39fd1bc00720c66cc6ced53d493e57a34939
thomasfouan <thomasfouan@hotmail.fr>
2018-11-12 14:44:59 +0100

    refactor removing unnecessary branches condition

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -23,21 +23,14 @@ class GildedRose {
             }
 
             if (items[i].quality > 0) {
-                if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                    items[i].quality = items[i].quality - 1;
-                }
+                items[i].quality = items[i].quality - 1;
             }
 
-            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                items[i].sellIn = items[i].sellIn - 1;
-            }
+            items[i].sellIn = items[i].sellIn - 1;
 
             if (items[i].sellIn < 0) {
                 if (items[i].quality > 0) {
-                    if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                        items[i].quality = items[i].quality - 1;
-                    }
-
+                    items[i].quality = items[i].quality - 1;
                 }
             }
         }
